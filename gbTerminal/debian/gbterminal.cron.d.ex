1b3bc510ab82b598c74e9895792feebb5d7fca40
Williams Lopez <williamsflc@gmail.com>
2018-10-29 11:19:08 -0600

    ELIMINANDO DATOS DE PAQUETES

--- gbTerminal/debian/gbterminal.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gbterminal package
-#
-0 4	* * *	root	[ -x /usr/bin/gbterminal_maintenance ] && /usr/bin/gbterminal_maintenance
--- gbTerminal/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gbterminal):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="gbterminal" command="/usr/bin/gbterminal"
